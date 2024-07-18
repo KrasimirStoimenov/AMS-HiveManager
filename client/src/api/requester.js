87258511c0a649ada589dec155f399a519bb94f2
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-07-18 22:41:41 +0300

    Added requester for fetch requests

---
+++ client/src/api/requester.js
@@ -0,0 +1,28 @@
+async function requester(method, url, data) {
+    const options = {};
+
+    if (data) {
+        options.method = method;
+        options.headers = {
+            'Content-Type': 'application/json'
+        }
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result;
+}
+
+const get = requester.bind(null, 'GET');
+const post = requester.bind(null, 'POST');
+const put = requester.bind(null, 'PUT');
+const del = requester.bind(null, 'DELETE');
+
+export default requester = {
+    get,
+    post,
+    put,
+    del
+}
