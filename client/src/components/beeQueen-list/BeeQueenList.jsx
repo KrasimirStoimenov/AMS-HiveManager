22158414b4f597dc535603828ed7402d61884263
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-07-23 15:54:43 +0300

    Added loader when fetching bee queens list

--- client/src/components/beeQueen-list/BeeQueenList.jsx
+++ client/src/components/beeQueen-list/BeeQueenList.jsx
@@ -8,15 +8,18 @@ import Button from 'react-bootstrap/esm/Button';
 import beeQueensAPI from '../../api/beeQueens-api';
 import BeeQueenListItem from './beeQueen-list-item/BeeQueenListItem';
 import { Col, Row } from 'react-bootstrap';
+import Loading from '../loading/Loading';
 
 export default function BeeQueenList() {
     const [beeQueens, setBeeQueens] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             const result = await beeQueensAPI.getAll();
 
             setBeeQueens(Object.values(result));
+            setIsLoading(false);
         })();
     }, []);
     return (
@@ -29,25 +32,28 @@ export default function BeeQueenList() {
                     <Button as={Link} to={'/beeQueen/add'} variant='outline-primary'><i className="bi bi-plus-lg"></i> Add Bee Queen</Button>
                 </Col>
             </Row>
-            <Table border={1}>
-                <thead>
-                    <tr>
-                        <th>Year</th>
-                        <th>Color Mark</th>
-                        <th>IsAlive</th>
-                        <th>HiveId</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {beeQueens.map(beeQueen =>
-                        <BeeQueenListItem
-                            key={beeQueen._id}
-                            beeQueen={beeQueen}
-                        />
-                    )}
-                </tbody>
-            </Table>
+            {isLoading
+                ? <Loading />
+                : <Table border={1}>
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Color Mark</th>
+                            <th>IsAlive</th>
+                            <th>HiveId</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {beeQueens.map(beeQueen =>
+                            <BeeQueenListItem
+                                key={beeQueen._id}
+                                beeQueen={beeQueen}
+                            />
+                        )}
+                    </tbody>
+                </Table>
+            }
         </Container>
     );
 };
