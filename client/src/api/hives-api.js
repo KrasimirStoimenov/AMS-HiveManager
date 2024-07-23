2a02bb87ff36fdcc672a213e80ee5dd99598bd13
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-07-23 13:46:41 +0300

    Added get by id api request

--- client/src/api/hives-api.js
+++ client/src/api/hives-api.js
@@ -3,10 +3,12 @@ import requester from './requester'
 const BASE_URL = `http://localhost:3030/jsonstore/hives`;
 
 const getAll = () => requester.get(`${BASE_URL}`);
+const getById = (hiveId) => requester.get(`${BASE_URL}/${hiveId}`);
 const add = (data) => requester.post(`${BASE_URL}`, data);
 
 const hivesAPI = {
     getAll,
+    getById,
     add,
 };
 
