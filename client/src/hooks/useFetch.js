d5083da6c17dfaebae01a4c1e7c4508b2719fd14
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-07-24 17:06:15 +0300

    Refetch functionality implemented for useFetch hook

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,18 +4,25 @@ import requester from "../api/requester";
 export function useFetch(url, initialData) {
     const [data, setData] = useState(initialData);
     const [isFetching, setIsFetching] = useState(true);
+    const [toggleRefetch, setToggleRefetch] = useState(false);
 
     useEffect(() => {
         (async () => {
             const result = await requester.get(url);
-                 
+
             setData(result);
             setIsFetching(false);
         })();
-    }, [url]);
+    }, [url, toggleRefetch]);
+
+    function refetch() {
+        setIsFetching(true);
+        setToggleRefetch(state => !state);
+    };
 
     return {
         data,
-        isFetching
+        isFetching,
+        refetch
     };
 }
