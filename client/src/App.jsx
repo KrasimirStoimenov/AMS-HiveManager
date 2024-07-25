93a70f363ad5e3a5e7e8056e9bf219d17d3826ed
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-07-25 15:41:18 +0300

    Create AuthContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,24 +13,40 @@ import HiveDetails from './components/hive-details/HiveDetails';
 import HiveAdd from './components/hive-add/HiveAdd';
 import BeeQueenList from './components/beeQueen-list/BeeQueenList';
 import BeeQueenAdd from './components/beeQueen-add/BeeQueenAdd';
+import { useState } from 'react';
+import { AuthContext } from './contexts/AuthContext';
+import Login from './components/login/Login';
 
 function App() {
-  return (
-    <>
-      <Header />
+    const [authState, setAuthState] = useState({});
 
-      <Container>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/apiaries/add' element={<ApiaryAdd />} />
-          <Route path='/hives/add' element={<HiveAdd />} />
-          <Route path='/hives/:hiveId/details' element={<HiveDetails />} />
-          <Route path='/beeQueens' element={<BeeQueenList />} />
-          <Route path='/beeQueens/add' element={<BeeQueenAdd />} />
-        </Routes>
-      </Container>
-    </>
-  )
+    const changeAuthState = (state) => {
+        setAuthState(state);
+    }
+    const contextData = {
+        email: authState.email,
+        accessToken: authState.accessToken,
+        isAuthenticated: !!authState.email,
+        changeAuthState
+    }
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            <Header />
+
+            <Container>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/apiaries/add' element={<ApiaryAdd />} />
+                    <Route path='/hives/add' element={<HiveAdd />} />
+                    <Route path='/hives/:hiveId/details' element={<HiveDetails />} />
+                    <Route path='/beeQueens' element={<BeeQueenList />} />
+                    <Route path='/beeQueens/add' element={<BeeQueenAdd />} />
+                </Routes>
+            </Container>
+        </AuthContext.Provider>
+    )
 }
 
 export default App
