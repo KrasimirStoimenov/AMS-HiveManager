85d9890ec954adbe8e9aa03fd7639cf753dacaf4
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-08-02 15:16:58 +0300

    Modified useForm custom hook to have ability for initialize and work with checkboxes

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,24 +3,29 @@ import { useState } from "react";
 export function useForm(initialValues, submitHandlerCallback) {
     const [values, setValues] = useState(initialValues);
 
-    function changeHandler(e) {
-        e.preventDefault();
-
+    const changeHandler = (e) => {
         setValues(prevState => ({
             ...prevState,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.type === 'checkbox'
+                ? e.target.checked
+                : e.target.value
         }));
     }
 
-    function submitHandler(e) {
+    const submitHandler = (e) => {
         e.preventDefault();
 
         submitHandlerCallback(values);
     }
 
+    const reinitializeValues = (values) => {
+        setValues(values);
+    }
+
     return {
         values,
         changeHandler,
-        submitHandler
+        submitHandler,
+        reinitializeValues
     };
 }
